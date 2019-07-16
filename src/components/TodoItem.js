2fd1b1d35ce6f195ca0a5923c3396eaf3793e3ad
Carl Kakisis <Messinias1@gmail.com>
2019-07-16 00:31:31 -0400

    completed

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -20,16 +20,23 @@ export class TodoItem extends Component {
     };
   };
 
-  markComplete = e => {
-    console.log(this.props);
-  };
+  //   markComplete = e => {
+  //     console.log(this.props);
+  //   };
 
   render() {
+    const { id, title } = this.props.todo;
     return (
       <div style={this.getStyle()}>
         <p>
-          <input type="checkbox" onChange={this.markComplete} />{" "}
-          {this.props.todo.title}
+          <input
+            type="checkbox"
+            onChange={this.props.markComplete.bind(this, id)}
+          />{" "}
+          {title}
+          <button onClick={this.props.delTodo.bind(this, id)} style={btnStyle}>
+            x
+          </button>
         </p>
       </div>
     );
@@ -38,7 +45,19 @@ export class TodoItem extends Component {
 
 // PropTypes
 TodoItem.propTypes = {
-  todo: PropTypes.object.isRequired
+  todo: PropTypes.object.isRequired,
+  markComplete: PropTypes.func.isRequired,
+  delTodo: PropTypes.func.isRequired
+};
+
+const btnStyle = {
+  background: "#ff0000",
+  color: "#fff",
+  border: "none",
+  padding: "5px 9px",
+  borderRadius: "50%",
+  cursor: "pointer",
+  float: "right"
 };
 
 export default TodoItem;
